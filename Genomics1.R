eb073665d7023f8fd1cc123b37a062350fd79cd3
Emine Ozsahin <54634534+emineozsahin@users.noreply.github.com>
2020-09-27 19:14:58 -0400

    Update Genomics1.R

--- Genomics1.R
+++ Genomics1.R
@@ -1,21 +1,6 @@
 
 setwd("/Users/emineozsahin/Documents/R_worksheets_winter2020/BINF6110_Genomic_Methods")
 
-N50 <- function(filename){
-  colnames = c ("name", "length")
-  scaffold = read.table(filename, col.names = colnames, sep ="\t")
-  scaffolds <- c(sort(scaffold$length, decreasing = T))
-  for (i in cumsum(as.numeric(sort(scaffold$length, decreasing = T)))) {
-    if (i < sum(scaffold$length)/2) {vector <- c(vector, i) } }
-  result = scaffolds[length(vector)]
-  result1 = plot(sort(scaffold$length), cumsum(as.numeric(sort(scaffold$length)), decreasing=T))
-  result2= plot(sort(scaffold$length), cumsum(as.numeric(sort(scaffold$length))))
-  result3= plot(1:length(scaffold$length), cumsum(as.numeric(sort(scaffold$length, decreasing=T))))
-  return(result) 
-  return(result1)
-  }
-
-
 # Triticum aestivum RefSeq: 22 chromosomes are present. There are no scaffolds in the headers only chromosomes but in the metadata they indicate 22 scaffolds are present and calculated Scaffold N50 is 709773743. Overal 
 # wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/900/519/105/GCA_900519105.1_iwgsc_refseqv1.0/GCA_900519105.1_iwgsc_refseqv1.0_genomic.fna.gz
 # gunzip GCA_900519105.1_iwgsc_refseqv1.0_genomic.fna.gz
@@ -31,6 +16,19 @@ N50 <- function(filename){
 
 # 4-	What is the n50 given in the paper/metadata?  Can you recreate this result with your own calculations?
 
+N50 <- function(filename){
+  colnames = c ("name", "length")
+  scaffold = read.table(filename, col.names = colnames, sep ="\t")
+  scaffolds <- c(sort(scaffold$length, decreasing = T))
+  for (i in cumsum(as.numeric(sort(scaffold$length, decreasing = T)))) {
+    if (i < sum(scaffold$length)/2) {vector <- c(vector, i) } }
+  result = scaffolds[length(vector)]
+  result1 = plot(sort(scaffold$length), cumsum(as.numeric(sort(scaffold$length)), decreasing=T))
+  result2= plot(sort(scaffold$length), cumsum(as.numeric(sort(scaffold$length))))
+  result3= plot(1:length(scaffold$length), cumsum(as.numeric(sort(scaffold$length, decreasing=T))))
+  return(result) 
+  return(result1)
+  }
 
 N50("wheat_chromosome.txt") # 709,773,743
 
@@ -43,29 +41,6 @@ echo "scale=5 ; $Nn_count / $total_count * 100" |bc
 lower_count=`grep -o [atgc] Org_Aviadenovirus_complete_genome_fetch.fasta | wc -l`
 upper_count=`grep -o [ATGC] Org_Aviadenovirus_complete_genome_fetch.fasta | wc -l`
 echo "scale = 5; $lower_count / $upper_count" |bc
-
-
-
-
-
-
-
-# Triticum aestivum 3B chromosome WGS project CBUC00000000 : all scaffolds 
-# wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/077/335/GCA_001077335.1_ASM107733v1/GCA_001077335.1_ASM107733v1_genomic.fna.gz
-# gunzip GCA_001077335.1_ASM107733v1_genomic.fna.gz
-#metadata : https://www.ncbi.nlm.nih.gov/assembly/GCA_001077335.1/
-#1-	How big is the genome?
-# grep "^[^>]" GCA_001077335.1_ASM107733v1_genomic.fna | tr -d "\n" | wc -c
-# 58,502,153 #58,502,153
-#2-	How many scaffolds are included?  Are there both chromosomes and scaffolds?
-# 1450 # There are only 1450 scaffolds
-# 3-	What is the average scaffold/chromosome length?  Plot scaffold length from largest to smallest.
-# awk '/^>/ {if (seqlen) {print seqlen}; printf $0"\t"; seqlen=0;next; }{ seqlen += length($0)}END{print seqlen}' GCA_001077335.1_ASM107733v1_genomic.fna > wheat_scaffold_sorted.txt
-# 4-	What is the n50 given in the paper/metadata?  Can you recreate this result with your own calculations?
-N50("wheat_scaffold_sorted.txt") # 213,933
-# 5-	What percentage of the genome is N or n?
-
-
 
 # Drosophila melanogaster : if I grep scaffolds I cannot find the same result with metadata. if I use scaffolds and chromosomes together then I get the same result. They used scaffolds and chromosomes together to calculate N50
 # wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz
@@ -94,10 +69,24 @@ plot(sort(scaffolds$length), cumsum(as.numeric(sort(scaffolds$length))), xlab = 
 
 plot(1:length(scaffolds$length), cumsum (as.numeric(sort(scaffolds$length, decreasing=T))), , xlab = "chromosome/scaffold length", ylab = "cumulative sum")
 
-
 hist(sort(scaffolds$length, decreasing = F), xlab = "Scaffold length", main = "Histogram of Scaffolds")
 
 plot(sort(scaffolds$length, decreasing = F),  main = "Scaffolds")
+
+# Triticum aestivum 3B chromosome WGS project CBUC00000000 : all scaffolds 
+# wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/001/077/335/GCA_001077335.1_ASM107733v1/GCA_001077335.1_ASM107733v1_genomic.fna.gz
+# gunzip GCA_001077335.1_ASM107733v1_genomic.fna.gz
+#metadata : https://www.ncbi.nlm.nih.gov/assembly/GCA_001077335.1/
+#1-	How big is the genome?
+# grep "^[^>]" GCA_001077335.1_ASM107733v1_genomic.fna | tr -d "\n" | wc -c
+# 58,502,153 #58,502,153
+#2-	How many scaffolds are included?  Are there both chromosomes and scaffolds?
+# 1450 # There are only 1450 scaffolds
+# 3-	What is the average scaffold/chromosome length?  Plot scaffold length from largest to smallest.
+# awk '/^>/ {if (seqlen) {print seqlen}; printf $0"\t"; seqlen=0;next; }{ seqlen += length($0)}END{print seqlen}' GCA_001077335.1_ASM107733v1_genomic.fna > wheat_scaffold_sorted.txt
+# 4-	What is the n50 given in the paper/metadata?  Can you recreate this result with your own calculations?
+N50("wheat_scaffold_sorted.txt") # 213,933
+# 5-	What percentage of the genome is N or n?
 
 
 
